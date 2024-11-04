cd15fdb4e596f5e7fd4a04d23d4e01c38c010c44
NShitrit <nshitrit@opentext.com>
2024-11-04 10:47:51 +0200

    add new result file

--- src/test/java/MyTest/MyStepdefs.java
+++ src/test/java/MyTest/MyStepdefs.java
@@ -50,11 +50,11 @@ public class MyStepdefs {
         //throw new cucumber.api.PendingException();
     }
 
-//    @Given("numberEven")
-//    public void numberEven() {
-//        // Write code here that turns the phrase above into concrete actions
-//        //throw new cucumber.api.PendingException();
-//    }
+    @Given("numberEven")
+    public void numberEven() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new cucumber.api.PendingException();
+    }
 
     @Then("one")
     public void oneThen() {
