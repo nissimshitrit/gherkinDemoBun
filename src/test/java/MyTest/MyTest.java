24aa07bc347571c02b021eec79758aaa1766b055
NShitrit <nshitrit@opentext.com>
2024-11-14 16:42:14 +0200

    add new result file

--- src/test/java/MyTest/MyTest.java
+++ src/test/java/MyTest/MyTest.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 //@CucumberOptions(plugin = "com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml",
 //        features = "src/test/resources/F1/" , tags = "not @exclude")
-@CucumberOptions(features = "src/test/resources/F1/"
-        ,glue = {"MyTest"}
-        //,plugin = { "junit:target/cucumber-reports.xml" }
+@CucumberOptions(features = "src/test/resources/F1",
+        glue = {"MyTest"}
+        //,
+        //plugin = { "junit:target/cucumber-reports.xml" }
         )
 public class MyTest {
 
